2e7546009dc5fc632e8f80dd979661436a9202d3
Sahil Kumar <sahil.kumar@excodelabs.com>
2026-09-12 08:45:52 +0530

    feat: create not found page ui

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,33 @@
+import { MoveLeft } from 'lucide-react';
+import image from '../images/Error.png';
+
+import { Link } from 'react-router-dom';
 const NotFound = () => {
   return (
-    <div>
-      <h1>Error! 404</h1>
-      <p>Not Found</p>
+    <div className="bg-[#F8FAFC] w-full h-[80vh] flex flex-col items-center">
+      <img className="w-[50%] h-[60%] object-cover mt-10" src={image} alt="" />
+
+      <h1 className="text-4xl font-bold text-black">Page not found</h1>
+
+      <p className="p-4 font-semibold text-[gray] text-xl">
+        The page you're looking for doesn't exist or may have been moved.
+      </p>
+
+      <div className="flex gap-4">
+        <Link
+          className="flex justify-center items-center gap-2 bg-indigo-600 text-white text-lg font-semibold mt-1 border-2 px-4 py-2 rounded-lg hover:bg-indigo-700 active:bg-indigo-800 transition-all duration-400"
+          to="/todos"
+        >
+          <MoveLeft /> Back to Todos{' '}
+        </Link>
+
+        <Link
+          className="text-indigo-600 text-lg font-semibold mt-1 border  px-4 py-2 rounded-lg hover:bg-indigo-50 active:bg-indigo-200 transition-all duration-400"
+          to="/"
+        >
+          Go to Home{' '}
+        </Link>
+      </div>
     </div>
   );
 };
