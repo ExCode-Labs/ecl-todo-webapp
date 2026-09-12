661fc05d162fecd09a79644a7ca07a45723a681a
Sahil Kumar <sahil.kumar@excodelabs.com>
2026-09-12 22:03:16 +0530

    feat: create not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,9 @@
 import { MoveLeft } from 'lucide-react';
-import image from '../images/Error.png';
-
 import { Link } from 'react-router-dom';
 const NotFound = () => {
   return (
     <div className="bg-[#F8FAFC] w-full h-[80vh] flex flex-col items-center">
-      <img className="w-[50%] h-[60%] object-cover mt-10" src={image} alt="" />
+      <img className="w-[50%] h-[60%] object-cover mt-10" src="/images/Error.png" alt="Not Found" />
 
       <h1 className="text-4xl font-bold text-black">Page not found</h1>
 
@@ -15,17 +13,17 @@ const NotFound = () => {
 
       <div className="flex gap-4">
         <Link
-          className="flex justify-center items-center gap-2 bg-indigo-600 text-white text-lg font-semibold mt-1 border-2 px-4 py-2 rounded-lg hover:bg-indigo-700 active:bg-indigo-800 transition-all duration-400"
+          className="flex justify-center items-center gap-2 bg-indigo-500 text-white text-lg font-semibold mt-1 border-2 px-4 py-2 rounded-lg hover:bg-indigo-600 active:bg-indigo-700 transition-all duration-400"
           to="/todos"
         >
-          <MoveLeft /> Back to Todos{' '}
+          <MoveLeft /> Back to Todos
         </Link>
 
         <Link
           className="text-indigo-600 text-lg font-semibold mt-1 border  px-4 py-2 rounded-lg hover:bg-indigo-50 active:bg-indigo-200 transition-all duration-400"
           to="/"
         >
-          Go to Home{' '}
+          Go to Home
         </Link>
       </div>
     </div>
